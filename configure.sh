5eaff9b158f5920434fde20ca4211bdcd160e1e2
Harry Reeder <harry@reeder.dev>
2019-03-16 07:49:30 -0400

    Auto updates

--- configure.sh
+++ configure.sh
@@ -6,6 +6,6 @@ tar xzf fly-5.0.0-linux-amd64.tgz
 echo "Configuring Concourse at $BASE_URL, using user $USERNAME"
 ./fly -t main login -c "$BASE_URL" -u "$USERNAME" -p "$PASSWORD"
 
-yes | ./fly -t main set-pipeline -p concourse-config -c pipelines/concourse-config.pipeline.yml
+yes | ./fly -t main set-pipeline -p concourse-config -c config/pipelines/concourse-config.pipeline.yml
 
 yes | ./fly -t main set-team -n harry-infra -c config/teams/harry-infra.yaml
