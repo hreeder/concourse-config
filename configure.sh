041316fd5ed855b8905ae4a10b130cbb420ac35c
Harry Reeder <harry@reeder.dev>
2019-03-15 20:06:03 -0400

    Add some basic output

--- configure.sh
+++ configure.sh
@@ -3,6 +3,7 @@ apk add ca-certificates
 wget https://github.com/concourse/concourse/releases/download/v5.0.0/fly-5.0.0-linux-amd64.tgz
 tar xzf fly-5.0.0-linux-amd64.tgz
 
-./fly -t main login -c $URL -u $USERNAME -p $PASSWORD
+echo "Configuring Concourse at $URL"
+./fly -t main login -c "$URL" -u "$USERNAME" -p "$PASSWORD"
 
 ./fly -t main set-team -n harry-infra -c config/teams/harry-infra.yaml
